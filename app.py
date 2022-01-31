934c2b6b2a0a6ae11d826b750adf7fed6644f925
ssohug <90553690+ssohug@users.noreply.github.com>
2022-01-31 09:03:03 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request
+from tensorflow import keras
+
+import cv2
+import numpy as np
+import pandas as pd
+
+app = Flask(__name__)
+
+kotha = keras.models.load_model('final_saved_model/')
+print("+"*50, "Model is loaded")
+
+labels = pd.read_csv("labels.txt", sep="\r\n").values
+
+@app.route('/')
+def index():
+	return render_template("index.html", data="hey")
+
+
+@app.route("/prediction", methods=["POST"])
+def prediction():
+
+	img = request.files['img']
+
+	img.save("img.jpg")
+
+	image = cv2.imread("img.jpg")
+
+	image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+	image = cv2.resize(image, (128,128))
+
+	image = np.reshape(image, (1,128,128,3))
+
+	pred = kotha.predict(image)
+
+	pred = np.argmax(pred)
+
+	pred = labels[pred]
+
+	return render_template("prediction.html", data=pred)
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
